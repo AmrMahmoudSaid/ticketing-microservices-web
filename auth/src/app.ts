63a1d5ded69f4bf0eac24fcbccd5bc67f7e682ac
amr mahmoud <amrmahmoud1900@gmail.com>
2024-03-16 02:25:44 +0200

    finish auth service

---
+++ auth/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors'
+import cookieSession from "cookie-session";
+import {json} from "body-parser";
+import mongoose from 'mongoose';
+import {currentUserRouter} from "./routes/currentuser";
+import {signinRouter} from "./routes/signin";
+import {signoutRouter} from "./routes/signout";
+import {signupRouter} from "./routes/signup";
+import {errorHandler} from "./middlewares/error-handler";
+import {NotFound} from "./errors/not-found";
+import * as tty from "tty";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false
+    // secure: true
+}))
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.all('*', () => {
+    throw new NotFound();
+})
+app.use(errorHandler);
+
+export {app};
