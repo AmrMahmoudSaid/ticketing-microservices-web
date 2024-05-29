b09eb5ec2a9ea129e4523be5ff73190ca16ea84f
amr mahmoud <amrmahmoud1900@gmail.com>
2024-05-29 20:51:50 +0300

    add delete order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,30 @@
 import express, {Request, Response} from "express";
+import {Order, OrderStatus} from "../models/order";
+import {NotAuthorizedError, NotFound, requireAuth} from "@amtickets377/common";
+import {OrderCreatedPublisher} from "../event/publisher/order-created-publisher";
+import {natsWrapper} from "../nats-wrapper";
+import {OrderCancelledPublisher} from "../event/publisher/order-cancelled-publisher";
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', async (req: Request, res:Response)=>{
-    res.send({});
+    const orderId = req.params.orderId;
+    const order = await Order.findById(orderId).populate('ticket');
+    if(!order){
+        throw new NotFound();
+    }
+    if (order.userId !== req.currentUser!.id){
+        throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id
+        }
+    });
+    res.status(204).send(order);
 });
 
 export {router as deleteOrderRouter};
